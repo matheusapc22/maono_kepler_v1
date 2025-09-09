f696ae5e8238c9b84c06a7a4cfc03909c0637fc5
Olamide Adeniyi <olami02bj@gmail.com>
2025-09-09 12:40:21 -0300

    updated site to only show share button for admin users

--- src/pages/Kepler/factories/panel-header.tsx
+++ src/pages/Kepler/factories/panel-header.tsx
@@ -3,15 +3,21 @@
 // @ts-nocheck
 
 import { PanelHeaderFactory, Icons } from "@kepler.gl/components";
-import { KEPLER_GL_VERSION } from "@kepler.gl/constants";
+import {
+  BUG_REPORT_LINK,
+  KEPLER_GL_VERSION,
+  USER_GUIDE_DOC,
+} from "@kepler.gl/constants";
 import Logo from "../../../assets/images/logo-2.png";
-import { connect } from "react-redux";
 
 export function CustomPanelHeaderFactory(...deps) {
   const PanelHeader = PanelHeaderFactory(...deps);
   const defaultActionItems = PanelHeader.defaultProps.actionItems;
 
-  console.log(defaultActionItems);
+  const queryString = window.location.search;
+  const params = new URLSearchParams(queryString);
+  const adminUserParameter = params.get("user");
+  const isAdminUser = adminUserParameter === "admin";
 
   const CustomKeplerLogo = () => {
     /*
@@ -27,51 +33,53 @@ export function CustomPanelHeaderFactory(...deps) {
     );
   };
 
-  const ShareButton = connect((root: any, own: any) => ({
-    rootState: root,
-    keplerGlId: own.id,
-  }))((props: any) => {
-    return (
-      <button
-        className="px-2 py-1 border rounded border-white text-white"
-        title="Share URL"
-        onClick={props?.onShareMap}
-      >
-        Share
-      </button>
-    );
-  });
+  // const ShareButton = connect((root: any, own: any) => ({
+  //   rootState: root,
+  //   keplerGlId: own.id,
+  // }))((props: any) => {
+  //   return (
+  //     <button
+  //       className="px-2 py-1 border rounded border-white text-white"
+  //       title="Share URL"
+  //       onClick={props?.onShareMap}
+  //     >
+  //       Share
+  //     </button>
+  //   );
+  // });
 
   PanelHeader.defaultProps = {
     ...PanelHeader.defaultProps,
     logoComponent: CustomKeplerLogo,
-    actionItems: [
-      // {
-      //   id: "bug",
-      //   iconComponent: Icons.Bug,
-      //   href: BUG_REPORT_LINK,
-      //   blank: true,
-      //   tooltip: "Bug Report",
-      //   onClick: () => {},
-      // },
-      // {
-      //   id: "docs",
-      //   iconComponent: Icons.Docs2,
-      //   href: USER_GUIDE_DOC,
-      //   blank: true,
-      //   tooltip: "User Guide",
-      //   onClick: () => {},
-      // },
-      // defaultActionItems.find((item) => item.id === "storage"),
-      {
-        // ...defaultActionItems.find((item) => item.id === "save"),
-        label: "",
-        tooltip: "Share",
-        id: "share-url-only",
-        dropdownComponent: (p: any) => <ShareButton {...p} />,
-        iconComponent: () => <></>,
-      },
-    ],
+    actionItems: isAdminUser
+      ? [
+          {
+            id: "bug",
+            iconComponent: Icons.Bug,
+            href: BUG_REPORT_LINK,
+            blank: true,
+            tooltip: "Bug Report",
+            onClick: () => {},
+          },
+          {
+            id: "docs",
+            iconComponent: Icons.Docs2,
+            href: USER_GUIDE_DOC,
+            blank: true,
+            tooltip: "User Guide",
+            onClick: () => {},
+          },
+          defaultActionItems.find((item) => item.id === "storage"),
+          {
+            ...defaultActionItems.find((item) => item.id === "save"),
+            label: "",
+            tooltip: "Share",
+            id: "share-url-only",
+            // dropdownComponent: (p: any) => <ShareButton {...p} />,
+            // iconComponent: () => <></>,
+          },
+        ]
+      : [],
   };
   return PanelHeader;
 }
