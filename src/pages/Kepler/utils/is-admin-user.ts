d93c11adbe93bab86683d65058ef2353b35f6553
Olamide Adeniyi <olami02bj@gmail.com>
2025-09-09 20:54:47 -0300

    changed admin user

--- src/pages/Kepler/utils/is-admin-user.ts
+++ src/pages/Kepler/utils/is-admin-user.ts
@@ -1,8 +1,10 @@
+const ADMIN_USER = "adminma426o123no321";
+
 const checkAdminUser = (): boolean => {
   const queryString = window.location.search;
   const params = new URLSearchParams(queryString);
   const adminUserParameter = params.get("user");
-  return adminUserParameter === "admin";
+  return adminUserParameter === ADMIN_USER;
 };
 
 export default checkAdminUser;
