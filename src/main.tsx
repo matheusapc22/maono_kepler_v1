59271d1875305c1db231f1d0d2afe511e7d1083f
matheusapc22 <matheusapc22@gmail.com>
2026-05-22 20:16:34 -0300

    Ativa identificador automático no painel

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import "./fallback-ui-styles";
+import "./auto-project-id";
 import App from "./App.tsx";
 import { Provider } from "react-redux";
 import store from "./store";
