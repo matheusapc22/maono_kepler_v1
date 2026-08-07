78a9a3145c1c5711f903cdd858b1e01119450b51
matheusapc22 <matheusapc22@gmail.com>
2026-08-07 11:18:57 -0300

    fix(ui): substitui marca visivel no banner legado

--- src/pages/Kepler/components/announcement.tsx
+++ src/pages/Kepler/components/announcement.tsx
@@ -32,7 +32,7 @@ export const FormLink = "https://shan990829.typeform.com/to/RbCAXt";
 const Announcement = ({ onDisable }) => (
   <StyledText>
     <span>
-      Kepler.gl turns two years old! Help our open source community by taking
+      Maõno turns two years old! Help our open source community by taking
       this 5-minute-survey and get a chance to win a{" "}
       <b>$100 Amazon gift card</b>. Make your answers count!
     </span>
