1f2246a8e1734f5f2aecc20c2acbb2c40909342a
matheusapc22 <matheusapc22@gmail.com>
2026-08-29 16:09:24 -0300

    refactor(map): remove botões nativos do canto superior direito

--- src/pages/Kepler/factories/map-control.tsx
+++ src/pages/Kepler/factories/map-control.tsx
@@ -1,108 +1,59 @@
 // SPDX-License-Identifier: MIT
-// Copyright contributors to the kepler.gl project
 // @ts-nocheck
 
 import styled from "styled-components";
 
-import {
-  withState,
-  MapControlFactory,
-  EffectControlFactory,
-  EffectManagerFactory,
-} from "@kepler.gl/components";
-import { AiAssistantControlFactory } from "@kepler.gl/ai-assistant";
+import { MapControlFactory } from "@kepler.gl/components";
 
-import {
-  BannerMapPanel,
-  SampleMapPanel,
-} from "../components/map-control/map-control";
-import SqlPanelControlFactory from "../components/map-control/sql-panel-control";
-
-const StyledMapControlPanel = styled.div`
-  position: relative;
-`;
-
-const StyledMapControlContextPanel = styled.div`
-  max-height: 100%;
-  overflow: hidden;
-
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  pointer-events: none !important; /* prevent padding from blocking input */
-  & > * {
-    /* all children should allow input */
-    pointer-events: all;
-  }
-`;
-
-const StyledMapControlOverlay = styled.div`
+/**
+ * A Maõno possui seus próprios controles de mapa. Mantemos o MapLegendPanel
+ * montado apenas para que a legenda customizada possa abrir via estado, mas o
+ * botão nativo correspondente e todos os demais botões do Kepler ficam fora da
+ * interface.
+ */
+const NativeControlHost = styled.div`
   position: absolute;
-  display: flex;
-  top: ${(props) => props.top}px;
+  top: 0;
   right: 0;
+  width: 0;
+  height: 0;
+  overflow: visible;
+  pointer-events: none;
   z-index: 1;
-  pointer-events: none !important; /* prevent padding from blocking input */
-  & > * {
-    /* all children should allow input */
-    pointer-events: all;
-  }
-
-  margin-top: ${(props) =>
-    props.rightPanelVisible ? props.theme.rightPanelMarginTop : 0}px;
-  margin-right: ${(props) =>
-    props.rightPanelVisible ? props.theme.rightPanelMarginRight : 0}px;
-  ${(props) => (props.fullHeight ? "height" : "max-height")}: calc(100% - ${(
-    props
-  ) =>
-    props.theme.rightPanelMarginTop + props.theme.bottomWidgetPaddingBottom}px);
 
   .map-control {
-    ${(props) => (props.rightPanelVisible ? "padding-top: 0px;" : "")}
+    width: 0;
+    height: 0;
+    padding: 0;
+    margin: 0;
+    overflow: visible;
+    pointer-events: none;
+  }
+
+  .map-control .map-control-button,
+  .map-control .show-legend {
+    display: none;
   }
 `;
 
-CustomMapControlFactory.deps = [
-  EffectControlFactory,
-  EffectManagerFactory,
-  SqlPanelControlFactory,
-  AiAssistantControlFactory,
-  ...MapControlFactory.deps,
-];
-function CustomMapControlFactory(
-  EffectControl,
-  EffectManager,
-  SqlPanelControl,
-  AiAssistantControl,
-  ...deps
-) {
+CustomMapControlFactory.deps = MapControlFactory.deps;
+
+function CustomMapControlFactory(...deps) {
   const MapControl = MapControlFactory(...deps);
-  const actionComponents = [
-    ...(MapControl.defaultActionComponents ?? []),
-    EffectControl,
-    SqlPanelControl,
-    AiAssistantControl,
-  ];
+  const MapLegendPanel = deps[3];
 
-  const CustomMapControl = (props) => {
-    const showEffects = Boolean(props.mapControls?.effect?.active);
-    return (
-      <StyledMapControlOverlay top={props.top} rightPanelVisible={showEffects}>
-        <StyledMapControlPanel>
-          {<BannerMapPanel {...props} />}
-          {!props.isExport && props.currentSample ? (
-            <SampleMapPanel {...props} />
-          ) : null}
-          <MapControl {...props} top={0} actionComponents={actionComponents} />
-        </StyledMapControlPanel>
-        <StyledMapControlContextPanel>
-          {showEffects ? <EffectManager /> : null}
-        </StyledMapControlContextPanel>
-      </StyledMapControlOverlay>
-    );
-  };
+  const CustomMapControl = (props) => (
+    <NativeControlHost data-maono-kepler-controls="hidden">
+      <MapControl
+        {...props}
+        top={0}
+        actionComponents={MapLegendPanel ? [MapLegendPanel] : []}
+      />
+    </NativeControlHost>
+  );
 
-  return withState([], (state) => ({ ...state.demo.app }))(CustomMapControl);
+  CustomMapControl.displayName = "MaonoHiddenNativeMapControls";
+  return CustomMapControl;
 }
 
 export function replaceMapControl() {
